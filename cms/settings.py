cef9df9637fe0ef5700512964d3b209ea954fc1b
ArtusU <artusrock@hotmail.com>
2020-11-30 09:27:17 +0000

    added django-heroku

--- cms/settings.py
+++ cms/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 
 
@@ -125,6 +126,9 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+django_heroku.settings(locals())
 
 
 
